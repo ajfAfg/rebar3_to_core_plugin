f85cdee7a910e797a787f0015c98a29d35396590
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-12-29 09:15:02 +0900

    select the better implementation of `do/1`

--- src/rebar3_to_core_plugin_prv.erl
+++ src/rebar3_to_core_plugin_prv.erl
@@ -20,86 +20,19 @@ init(State) ->
                           {opts, []},                   % list of options understood by the plugin
                           {short_desc, "A rebar plugin"},
                           {desc, "A rebar plugin"}]),
-    % NewState = rebar_state:set(State, erl_opts, to_core),
-    % {ok, rebar_state:add_provider(NewState, Provider)}.
     {ok, rebar_state:add_provider(State, Provider)}.
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    % ErlOpts = rebar_state:get(State, erl_opts, []),
-    % NewState = rebar_state:set(State, erl_opts, [to_core | ErlOpts]),
-    NewState = set_erl_opts(State),
-    NewState =
-        begin
-            UpdateErlOpts =
-                fun(AppInfo) ->
-                   ErlOpts = rebar_app_info:get(AppInfo, erl_opts, []),
-                   rebar_app_info:set(AppInfo, erl_opts, [to_core | ErlOpts])
-                end,
-            NewProjectApps = lists:map(UpdateErlOpts, rebar_state:project_apps(State)),
-            F = fun(App, State) -> rebar_state:project_apps(State, App) end,
-            lists:foldl(F, State, NewProjectApps)
+    UpdateErlOpts =
+        fun(AppInfo) ->
+           ErlOpts = rebar_app_info:get(AppInfo, erl_opts, []),
+           rebar_app_info:set(AppInfo, erl_opts, [to_core | ErlOpts])
         end,
-    io:format("State:~n~p~n", [State]),
-    io:format("NewState:~n~p~n", [NewState]),
+    NewProjectApps = lists:map(UpdateErlOpts, rebar_state:project_apps(State)),
+    F = fun(App, State) -> rebar_state:project_apps(State, App) end,
+    NewState = lists:foldl(F, State, NewProjectApps),
     rebar_prv_compile:do(NewState).
 
 -spec format_error(any()) -> iolist().
 format_error(Reason) -> rebar_prv_compile:format_error(Reason).
-
-%% ===================================================================
-%% Private API
-%% ===================================================================
-set_erl_opts({state_t,
-              Dir,
-              Opts,
-              CodePaths,
-              Default,
-              EscriptPath,
-              Lock,
-              CurrentProfiles,
-              Namespace,
-              CommandArgs,
-              CommandParsedArgs,
-              CurrentApp,
-              ProjectApps,
-              DepsToBuild,
-              AllPluginDeps,
-              AllDeps,
-              Compilers,
-              ProjectBuilders,
-              Resources,
-              Providers,
-              AllowProviderOverrides}) ->
-    % NewOpts = dict:append(erl_opts, to_core, Opts),
-    % NewCodePaths = dict:append(erl_opts, to_core, CodePaths),
-    % NewDefault = dict:append(erl_opts, to_core, Default),
-    F = fun(AppInfo) ->
-           ErlOpts = rebar_app_info:get(AppInfo, erl_opts, []),
-           rebar_app_info:set(AppInfo, erl_opts, [to_core | ErlOpts])
-        end,
-    NewProjectApps = lists:map(F, ProjectApps),
-    {state_t,
-     Dir,
-     %  NewOpts,
-     %  NewCodePaths,
-     %  NewDefault,
-     Opts,
-     CodePaths,
-     Default,
-     EscriptPath,
-     Lock,
-     CurrentProfiles,
-     Namespace,
-     CommandArgs,
-     CommandParsedArgs,
-     CurrentApp,
-     NewProjectApps,
-     DepsToBuild,
-     AllPluginDeps,
-     AllDeps,
-     Compilers,
-     ProjectBuilders,
-     Resources,
-     Providers,
-     AllowProviderOverrides}.
